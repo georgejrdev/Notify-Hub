a481ed98a44281a59acd20eb3078cea368eae505
NoxusJr <ctt.georgejr@gmail.com>
2024-06-10 03:55:32 -0300

    Implemented Interface in SendEmail

---
+++ Code/src/main/java/com/georgejrdev/core/SendEmail.java
@@ -0,0 +1,132 @@
+package com.georgejrdev.core;
+
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+import com.georgejrdev.core.interfaces.InterfaceSendEmail;
+
+
+public class SendEmail implements InterfaceSendEmail{
+    
+    private String emailServer;
+    private String emailServerHost;
+    private Session session;
+    private String email;
+    private String password;
+
+
+    public SendEmail() throws Exception{
+        setEmailServer(System.getenv("EMAIL_SERVER"));
+        setEmail();
+        setPassword();
+        configureEmailHost();
+        configureEmailSending();
+    }
+
+
+    @Override
+    public void sendNewEmail(String subject, String body,String to) {
+
+        try {
+            Message message = new MimeMessage(getSession());
+            message.setFrom(new InternetAddress(to));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            message.setSubject(subject);
+            message.setText(body);
+
+            Transport.send(message);
+
+            System.out.println("Email Enviado");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private void configureEmailHost() throws Exception{
+
+        switch (this.emailServer.toLowerCase()) {
+            case "gmail":
+                setEmailServerHost("smtp.gmail.com");
+                break;
+        
+            case "outlook":
+                setEmailServerHost("smtp.office365.com");
+                break;
+
+            case "yahoo":
+                setEmailServerHost("smtp.mail.yahoo.com");
+                break;
+            
+            case "zoho":
+                setEmailServerHost("smtp.zoho.com");
+                break;
+
+            default:
+                throw new Exception("Email Server Not Found");
+        }                
+    }
+
+
+    private void configureEmailSending(){
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", getEmailServerHost());
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        setSession( Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(getEmail(),getPassword());
+            }
+        }));
+    }
+
+
+    private void setEmailServer(String emailServer){
+        this.emailServer = emailServer;
+    }
+
+
+    @SuppressWarnings("unused")
+    private String getEmailServer(){
+        return this.emailServer;
+    }
+
+
+    private void setEmailServerHost(String emailServerHost){
+        this.emailServerHost = emailServerHost;
+    }
+
+
+    private String getEmailServerHost(){
+        return this.emailServerHost;
+    }
+
+    private void setSession(Session session){
+        this.session = session;
+    }
+
+    private Session getSession(){
+        return this.session;
+    }
+
+    private void setEmail(){
+        this.email = System.getenv("EMAIL");
+    }
+
+    private String getEmail(){
+        return this.email;
+    }
+
+    private void setPassword(){
+        this.password = System.getenv("EMAIL_APP_PASSWORD");
+    }
+
+    private String getPassword(){
+        return this.password;
+    }
+}
